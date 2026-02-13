a7e49f6fcaf9d136d8277c334e100cf10e7bebf2
Marcus <johansson.marcus89@hotmail.se>
2026-02-13 09:24:09 +0100

    Add project files.

---
+++ MenuActions.cs
@@ -0,0 +1,23 @@
+﻿
+internal class MenuActions
+{
+    internal static void GenerateData()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void ListCards()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void ListPeople()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void ListTransactions()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿while (true)
+{
+    Console.Clear();
+    Console.WriteLine("What do you want to do?");
+    Console.WriteLine("1. Generate mock data");
+    Console.WriteLine("2. List people");
+    Console.WriteLine("3. List Cards");
+    Console.WriteLine("4. List Transactions");
+
+
+    Console.Write("Choice: ");
+    var choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            MenuActions.GenerateData();
+            break;
+
+        case "2":
+            MenuActions.ListPeople();
+            break;
+
+        case "3":
+            MenuActions.ListCards();
+            break;
+
+        case "4":
+            MenuActions.ListTransactions();
+            break;
+
+
+        default:
+            Console.WriteLine("Invalid option!");
+            Console.ReadKey();
+            break;
+    }
+}
